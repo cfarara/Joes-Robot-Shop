cba1dd11f5fd9400f645af9e54fce36615a36077
Caleb Farara <caleb.farara@gmail.com>
2024-06-22 22:54:33 -0400

    We learned to create redirect routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { CartComponent } from './cart/cart.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Joe's Robot Shop" },
   { path: 'catalog', component: CatalogComponent, title: "Catalog - Joe's Robot Shop" },
-  { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" }
+  { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
 
 @NgModule({
